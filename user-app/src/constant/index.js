949190437e077025255486279867eefaec5b382f
Bolormaa222 <bolormaaenkhbat222@gmail.com>
2024-12-26 17:02:00 +0800

    commita

--- user-app/src/constant/index.js
+++ user-app/src/constant/index.js
@@ -1,6 +1,6 @@
 export default {
     "imageUrl":"https://upload-wps4.onrender.com/image",
     "videoUrl":"https://upload-wps4.onrender.com/upload",
-    "serverUrl":"http://localhost:7000"
+    "serverUrl":"https://elearnclient.onrender.com"
 }
 //"serverUrl":"https://elearnclient.onrender.com"
